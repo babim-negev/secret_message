842d8014fd87917f9c4297f6bfb6fdec9760bc6b
babim <captainprice1356@gmail.com>
2024-01-18 15:00:56 +0400

    shitcodding babim

--- src/api/settings.py
+++ src/api/settings.py
@@ -3,18 +3,8 @@ import os
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
-# ЗАГРУЖАЕМ ДАННЫЕ ИЗ .ENV
+
 load_dotenv()
-
-# ИЗВЛЕКАЕМ ДАННЫЕ
-db_host = os.getenv("POSTGRES_HOST")
-db_port = os.getenv("POSTGRES_PORT")
-db_name = os.getenv("POSTGRES_DB")
-db_user = os.getenv("POSTGRES_USER")
-db_password = os.getenv("POSTGRES_PASSWORD")
-
-POSTGRES_URL = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
-
 
 class DbSettings(BaseSettings):
     db_host: str = os.getenv("POSTGRES_HOST")
